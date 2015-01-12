c2563a27907b313ac88a04b90f03bd069088cff6
Andrey Vasenin <vaseninlol@gmail.com>
2015-01-12 15:41:05 +0600

    Freeze deps versions

--- Rakefile.rb
+++ Rakefile.rb
@@ -10,16 +10,18 @@ ENV['COPYFILE_DISABLE'] = 'true'
 # Gemspec
 gemspec = Gem::Specification.new do |s|
   s.name = 'echo-riemann-postgres-tool'
-  s.version = '0.1.7'
+  s.version = '0.1.9'
   s.author = 'Kyle Kingsbury'
   s.email = 'aphyr@aphyr.com'
   s.homepage = 'https://github.com/EchoTeam/riemann-tools'
   s.platform = Gem::Platform::RUBY
   s.summary = 'Utilities which submit events to Riemann.'
 
-  s.add_dependency 'riemann-client', '>= 0.2.2'
-  s.add_dependency 'trollop', '>= 1.16.2'
-  s.add_dependency 'pg', '>= 0.17.0'
+
+  s.add_dependency 'trollop', '2.0'
+  s.add_dependency 'beefcake', '0.5.0'
+  s.add_dependency 'riemann-client', '0.2.3'
+  s.add_dependency 'pg', '0.17.1'
 
 
   s.files = FileList['lib/**/*', 'bin/*', 'LICENSE', 'README.markdown'].to_a
